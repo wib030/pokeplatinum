fa0bdb461a679978b46e5995d34a727637f3af8a
motelegy <motselegy@gmail.com>
2024-07-19 17:44:54 -0500

    Testing exactly 1 thing on trainer_ai.c to see if we can make little changes without it breaking horribly. This change tries to add Shed Shell information to the AI

--- src/battle/trainer_ai/trainer_ai.c
+++ src/battle/trainer_ai/trainer_ai.c
@@ -4030,7 +4030,8 @@ static BOOL TrainerAI_ShouldSwitch(BattleSystem *battleSys, BattleContext *battl
             || BattleSystem_CountAbility(battleSys, battleCtx, COUNT_ALL_BATTLERS_THEIR_SIDE, battler, ABILITY_SHADOW_TAG)
             || BattleSystem_CountAbility(battleSys, battleCtx, COUNT_ALL_BATTLERS_THEIR_SIDE, battler, ABILITY_ARENA_TRAP)
             || (BattleSystem_CountAbility(battleSys, battleCtx, COUNT_ALL_BATTLERS_EXCEPT_ME, battler, ABILITY_MAGNET_PULL)
-                && MON_HAS_TYPE(battler, TYPE_STEEL))) {
+                && MON_HAS_TYPE(battler, TYPE_STEEL))
+            && battleCtx-BattleMons[battler].heldItem != ITEM_SHED_SHELL) {
         return FALSE;
     }
 
