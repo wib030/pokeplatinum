8d216f75bcd55030ffaa188e6fc0c38e19e134b0
wib030 <localizedlyss@gmail.com>
2025-02-28 17:36:17 +1100

    remove un-needed subscript and update pregnancy punch effect script

--- res/battle/scripts/effects/effect_script_0264.s
+++ res/battle/scripts/effects/effect_script_0264.s
@@ -3,7 +3,7 @@
     .data
 
 _000:
-	UpdateVar OPCODE_SET, BTLVAR_SIDE_EFFECT_FLAGS_INDIRECT, MOVE_SIDE_EFFECT_ON_HIT|MOVE_SUBSCRIPT_PTR_PREGNANCY_PUNCH
+	PregnancyPunch
 	CalcCrit 
     CalcDamage
     End 
