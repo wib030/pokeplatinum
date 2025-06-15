76cfe113a0440c8cd47c1b300cd0a6e577649e49
wib030 <localizedlyss@gmail.com>
2025-06-16 09:07:43 +1000

    Update subscript_safari_escape.s

--- res/battle/scripts/subscripts/subscript_safari_escape.s
+++ res/battle/scripts/subscripts/subscript_safari_escape.s
@@ -3,8 +3,11 @@
     .data
 
 _000:
+	CompareMonDataToValue OPCODE_EQU, BTLVAR_DEFENDER, BATTLEMON_CUR_HP, 0, _010
     // {0} is watching carefully!
     PrintGlobalMessage pl_msg_00000368_00849, TAG_NICKNAME, BTLSCR_DEFENDER
     Wait 
     WaitButtonABTime 30
-    End 
+	
+_010:
+	End
