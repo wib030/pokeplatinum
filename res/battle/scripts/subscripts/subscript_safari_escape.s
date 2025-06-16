cc14d9e33fcc4dbddcdc70572bddece7bba0613a
wib030 <localizedlyss@gmail.com>
2025-06-16 11:42:44 +1000

    safari add smooch and fix some messages

--- res/battle/scripts/subscripts/subscript_safari_escape.s
+++ res/battle/scripts/subscripts/subscript_safari_escape.s
@@ -3,7 +3,8 @@
     .data
 
 _000:
-	CompareMonDataToValue OPCODE_EQU, BTLVAR_DEFENDER, BATTLEMON_CUR_HP, 0, _010
+	CompareMonDataToValue OPCODE_EQU, BTLSCR_DEFENDER, BATTLEMON_CUR_HP, 0, _010
+	CompareMonDataToValue OPCODE_FLAG_SET, BTLSCR_DEFENDER, BATTLEMON_STATUS, MON_CONDITION_SLEEP, _015
     // {0} is watching carefully!
     PrintGlobalMessage pl_msg_00000368_00849, TAG_NICKNAME, BTLSCR_DEFENDER
     Wait 
@@ -11,3 +12,12 @@ _000:
 	
 _010:
 	End
+
+_015:
+	// {0} is fast asleep.
+    PrintMessage pl_msg_00000368_00299, TAG_NICKNAME, BTLSCR_DEFENDER
+    Wait 
+    WaitButtonABTime 30
+    PlayBattleAnimation BTLSCR_DEFENDER, BATTLE_ANIMATION_ASLEEP
+    Wait 
+    End 
