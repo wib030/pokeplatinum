55cd6760a0ac9b270e1a995fcb550a2f8dc552f7
wib030 <localizedlyss@gmail.com>
2025-07-24 12:56:00 +1000

    Update subscript_color_change_before.s

--- res/battle/scripts/subscripts/subscript_color_change_before.s
+++ res/battle/scripts/subscripts/subscript_color_change_before.s
@@ -3,7 +3,13 @@
     .data
 
 _000:
-     // {0}’s {1} made it the {2} type!
+	PlaySound BTLSCR_DEFENDER, 1980
+    SetMosaic BTLSCR_DEFENDER, 8, 1
+    Wait 
+    PlaySound BTLSCR_DEFENDER, 1984
+    SetMosaic BTLSCR_DEFENDER, 0, 1
+    Wait 
+    // {0}’s {1} made it the {2} type!
     PrintMessage pl_msg_00000368_00641, TAG_NICKNAME_ABILITY_TYPE, BTLSCR_DEFENDER, BTLSCR_DEFENDER, BTLSCR_MSG_TEMP
     Wait 
     WaitButtonABTime 30
