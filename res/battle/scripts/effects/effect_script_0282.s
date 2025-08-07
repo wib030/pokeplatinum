08f5f5202f54b351038913496b47ad41672cceb5
wib030 <localizedlyss@gmail.com>
2025-08-08 00:18:03 +1000

    implement hex power boost
    totally forgot to add this earlier teehee

--- res/battle/scripts/effects/effect_script_0282.s
+++ res/battle/scripts/effects/effect_script_0282.s
@@ -3,6 +3,10 @@
     .data
 
 _000:
+	CompareMonDataToValue OPCODE_FLAG_NOT, BTLSCR_DEFENDER, BATTLEMON_STATUS, MON_CONDITION_ANY, _010
+	UpdateVar OPCODE_SET, BTLVAR_POWER_MULTI, 20
+	
+_010:
     CalcCrit 
     CalcDamage 
     End 
