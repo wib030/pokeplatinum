fcbcc45800a36280ebeb8f75c4c549fa3592c5ec
wib030 <localizedlyss@gmail.com>
2025-10-01 11:48:34 +1000

    add new files

---
+++ include/battle/trainer_ai_switch_predict.h
@@ -0,0 +1,7 @@
+#ifndef POKEPLATINUM_BATTLE_TRAINER_AI_SWITCH_PREDICT_H
+#define POKEPLATINUM_BATTLE_TRAINER_AI_SWITCH_PREDICT_H
+
+#include "struct_decls/battle_system.h"
+#include "battle/battle_context.h"
+
+#endif // POKEPLATINUM_BATTLE_TRAINER_AI_SWITCH_PREDICT_H
---
+++ src/battle/trainer_ai/trainer_ai_switch_predict.c
@@ -0,0 +1,7 @@
+#include <nitro.h>
+#include <string.h>
+
+#include "pch/global_pch.h"
+#include "assert.h"
+
+#include "battle/trainer_ai_switch_predict.h"
